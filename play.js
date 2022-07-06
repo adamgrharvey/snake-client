efee937817fa208d64eebeb3e46a56989b111dbc
Adam <adamgrharvey@gmail.com>
2022-07-06 12:59:32 -0700

    split files into play and client

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
 const net = require("net");
+const connect = require('./client.js');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port:  50541
-  });
-
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(data)
-  });
-  
-  return conn;
-};
-
-
-// interpret incoming data as text
-
-
-console.log("Connecting ...");
 connect();
