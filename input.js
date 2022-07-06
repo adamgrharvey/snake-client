34c31cea251f327175f7e819ebd43fa348c5fc54
Adam <adamgrharvey@gmail.com>
2022-07-06 14:03:25 -0700

    adds ability to send messages

--- input.js
+++ input.js
@@ -18,21 +18,34 @@ const handleUserInput = function (key) {
     console.log("Exiting ...\n");
     process.exit();
   }
-  else if (key === 'w') {
+  else if (key === 'w' || key === 'W') {
     connection.write(`Move: up`)
     console.log(`Move: up`)
   }
-  else if (key === 'a') {
+  else if (key === 'a' || key === 'A') {
     connection.write(`Move: left`)
     console.log(`Move: left`)
   }
-  else if (key === 's') {
+  else if (key === 's' || key === 'S') {
     connection.write(`Move: down`)
     console.log(`Move: down`)
   }
-  else if (key === 'd') {
+  else if (key === 'd' || key === 'D') {
     connection.write(`Move: right`)
     console.log(`Move: right`)
+  }
+
+  else if (key === '1') {
+    connection.write(`Say: I'M`)
+    console.log(`Say: "Im"`)
+  }
+  else if (key === '2') {
+    connection.write(`Say: THE`)
+    console.log(`Say: THE`)
+  }
+  else if (key === '3') {
+    connection.write(`Say: BEST`)
+    console.log(`Say: BEST`)
   }
 };
 
